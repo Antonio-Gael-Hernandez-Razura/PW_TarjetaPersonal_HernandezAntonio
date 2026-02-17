173f4069eb9f4b1d95841f32086391ab38437761
Antonio <za230110346@zapopan.tecmm.edu.mx>
2026-02-17 16:32:45 -0600

    ligera correcion

--- scripts.js
+++ scripts.js
@@ -1,13 +1,8 @@
 const btnTema = document.getElementById("btnTema");
-const btnReceta = document.getElementById("btnReceta");
 const card = document.getElementById("card");
 
 btnTema.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
-});
-
-btnReceta.addEventListener("click", () => {
-    alert("Mi receta favorita es la lasaña 🍝");
 });
 
 card.addEventListener("mouseover", () => {
